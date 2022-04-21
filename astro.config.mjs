b986ce339b70174e02bfd4c69dcf55d9846c04c9
boomermath <boomermath13@gmail.com>
2022-04-21 12:35:55 +0000

    Edit Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import vue from '@astrojs/vue';
-
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  outDir: "https://boomermath.xyz",
+  site: "https://boomermath.xyz",
   integrations: [
-    react(),
-    vue(),
     tailwind()
   ]
 });
